339c5e83b88d21a6f1f1a30fabb7bd509f6a9e3e
guiiBecker <guilhermegbecker@gmail.com>
2025-04-25 16:54:49 -0300

    chore(docker): adicionado docker compose

---
+++ src/utils/getToken.py
@@ -0,0 +1,15 @@
+
+import os
+from dotenv import load_dotenv, find_dotenv
+
+def get_token():
+    load_dotenv(find_dotenv())
+    token = os.getenv("PANDASCORE_TOKEN")
+    if not token:
+        raise RuntimeError("PANDASCORE_TOKEN não definido!")
+    print(token)
+    return token
+
+
+if __name__ == "__main__":
+    get_token()
